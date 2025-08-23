431294c1e26d5904af5e7b296e4ff2a6fcceeb1d
aashish-j <aashish.jha@marbles.health>
2025-08-24 03:10:35 +0530

    trivial

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:mood_surge/app/app.dart';
 import 'package:mood_surge/bootstrap.dart';
 
-void main() {
-  bootstrap(() => const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+  await bootstrap(() => const App());
 }
